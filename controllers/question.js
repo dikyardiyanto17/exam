d7b54f0e6e66de2c047792c3434e58658664fdb6
dikyardiyanto17 <dikyardiyanto17@gmail.com>
2025-10-17 11:05:20 +0700

    add route for zipped

--- controllers/question.js
+++ controllers/question.js
@@ -1,6 +1,7 @@
 const { RedisDB } = require("../config/redis")
 const { Helpers } = require("../helpers")
 const Questions = require("../schema/Questions")
+const zlib = require("zlib")
 
 class Question {
 	static async index(req, res, next) {
@@ -18,8 +19,7 @@ class Question {
 				})
 			}
 
-			console.log("🧠 Cache miss — querying MongoDB")
-			const questions = await Questions.find()
+			const questions = await Questions.find({}, { _id: 1, title: 1, description: 1 })
 
 			// Save to Redis with expiry 2 hour
 			await redis.set(cacheKey, JSON.stringify(questions), { EX: 7200 })
@@ -33,6 +33,49 @@ class Question {
 			next(error)
 		}
 	}
+
+	static async index_zip(req, res, next) {
+		try {
+			const redis = RedisDB.getClient()
+			const cacheKey = "question_zip:all"
+
+			let cached
+			try {
+				cached = await redis.get(cacheKey)
+				if (cached) {
+					const data = JSON.parse(cached)
+					console.log("📦 Cache hit — returning from Redis")
+					return res.status(Helpers.RESPONSESUCCESS.CODE_200.statusCode).json({
+						...Helpers.RESPONSESUCCESS.CODE_200,
+						message: "Successfully Retrieved Data (from cache)",
+						data,
+					})
+				}
+			} catch (err) {
+				console.warn("⚠️ Redis read error or invalid cache:", err.message)
+				await redis.del(cacheKey).catch(() => {})
+			}
+
+			// Cache miss — query MongoDB
+			const questions = await Questions.find({}, { _id: 1, title: 1, description: 1 })
+
+			// Save to Redis with expiry 2 hours
+			try {
+				await redis.set(cacheKey, JSON.stringify(questions), { EX: 7200 })
+			} catch (err) {
+				console.error("Redis set error:", err.message)
+			}
+
+			return res.status(Helpers.RESPONSESUCCESS.CODE_200.statusCode).json({
+				...Helpers.RESPONSESUCCESS.CODE_200,
+				message: "Successfully Retrieved Data (from DB)",
+				data: questions,
+			})
+		} catch (error) {
+			next(error)
+		}
+	}
+
 	static async post(req, res, next) {
 		try {
 			await Questions.create({ questionId: "questionid", question: "Hallo Apa kabar ?", answer: "A", type: "choice" })
